a0996ce14f5139818fcd6c913fd4d3ce7480a522
salo12 <salomegodoy12@gmail.com>
2017-11-26 18:01:10 -0300

    Revert "Revert "Revert "Revert "Revert "Revert "funciones de pdf""""""
    This reverts commit 419b5c297a6e98c81a48fcc45d676bbe940c9d6b.

--- app/controllers/transfer_cases_reports_controller.rb
+++ app/controllers/transfer_cases_reports_controller.rb
@@ -2,13 +2,7 @@ class TransferCasesReportsController < ApplicationController
 	#para el formato pdf
     def index
     	@transferencia = TransferCase.order("id DESC").all
-		respond_to do |format|
-			format.html
-			format.pdf do
-				pdf = TransferCasePdf.new()				
-				send_data pdf.render, filename: 'Historial_de_Imputados.pdf', type: 'application/pdf', disposition: "inline"
-            end       
-         end
+		
     end
 
 	def listar_transferencias
