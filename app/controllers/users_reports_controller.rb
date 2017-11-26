419b5c297a6e98c81a48fcc45d676bbe940c9d6b
salo12 <salomegodoy12@gmail.com>
2017-11-26 18:01:06 -0300

    Revert "Revert "Revert "Revert "Revert "funciones de pdf"""""
    This reverts commit 586f0936f6cccdc4c624ff4a79dd5b04e55cc5dd.

--- app/controllers/users_reports_controller.rb
+++ app/controllers/users_reports_controller.rb
@@ -2,7 +2,13 @@ class UsersReportsController < ApplicationController
 	#para el formato pdf
     def index
     	@usuario = User.order("id DESC").all
-		
+		respond_to do |format|
+			format.html
+			format.pdf do
+				pdf = UserPdf.new()				
+				send_data pdf.render, filename: 'Reporte_de_Usuario.pdf', type: 'application/pdf', disposition: "inline"
+            end       
+         end
     end
 
 	def listar_defensores
